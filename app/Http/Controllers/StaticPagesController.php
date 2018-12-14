2d061f616d1b4a88904514390cfd6f7094bfd20f
slgogo <860900086@qq.com>
2018-12-14 02:59:57 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //
+
+    function home(){
+        return view('static_pages/home');
+    }
+    function about(){
+        return view('static_pages/about');
+    }
+    function help(){
+        return view('static_pages/help');
+    }
+}
