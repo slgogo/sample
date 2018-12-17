b51b0983a9b3c635519c55ef237cc1b05cf787e6
slgogo <860900086@qq.com>
2018-12-17 06:47:20 +0000

    用户注册表单

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
 
-Route::get('/users/{user}','UsersController@show')->name('users.show');
+// Route::get('/users/{user}','UsersController@show')->name('users.show');
+
+Route::resource('users','UsersController');
