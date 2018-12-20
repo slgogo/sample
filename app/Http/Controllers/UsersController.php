d13e18096c5f86cd92894d181f9860b6a38c8372
slgogo <860900086@qq.com>
2018-12-20 03:36:50 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,4 +32,29 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
 
     }
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+         // 验证用户更新的表单输入
+        $data = [];
+        // 新建一个空数组用于存放更改后的内容
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        // 判断如果更新时有新密码才获取新密码存储到临时数组$data
+        $user->update($data);
+        // 把获取到更新的表单项目内容提交到对应的模型实例
+        session()->flash('success', '个人资料更新成功！');
+        // 更新完成显示提示信息
+        return redirect()->route('users.show', $user);
+        // 更新完成重定向到用户资料页面并传入用户数据给当前视图
+    }
+    // 通过用户模型实例$user的update()方法，更新表单内容到数据库
 }
