5f23ee761a6efd4948a77d727ac64cb1b7c2975d
slgogo <860900086@qq.com>
2019-01-03 03:20:25 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
+
 
 class StaticPagesController extends Controller
 {
     //
 
     function home(){
-        return view('static_pages/home');
+
+        $feed_items = [];
+        if (Auth::check()) {
+            // 检查是否登陆
+            $feed_items = Auth::user()->feed()->paginate(30);
+            // 当前用户的所有微博信息并分页
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
+
     function about(){
         return view('static_pages/about');
     }
+
     function help(){
         return view('static_pages/help');
     }
